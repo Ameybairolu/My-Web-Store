88c17a4301f6d14be5f85cb4a965cb398f9c6f01
Ameybairolu <71092384+Ameybairolu@users.noreply.github.com>
2021-08-29 14:31:57 +0530

    Delete AmeyBairolu directory

--- AmeyBairolu/includes/footer.php
+++
@@ -1,8 +0,0 @@
-<!--The footer that will be used in all the pages-->
-<footer> 
-    <div class="container"> 
-        <center> 
-            <p>Copyright &copy; Lifestyle Store. All Rights Reserved | Contact Us: +91 90000 00000</p> 
-        </center> 
-    </div> 
-</footer>
